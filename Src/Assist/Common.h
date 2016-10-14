514962dfe4f7ecf7ed4840172fa34a2945e68377
bnagybalint <b.nagy.balint@gmail.com>
2016-10-14 20:22:09 +0200

    Removing Assert warning

--- Src/Assist/Common.h
+++ Src/Assist/Common.h
@@ -42,7 +42,7 @@ void RuntimeAssert(bool cond);
 #ifdef BDE_GLOBAL_BUILD_DEBUG
 # define Assert( cond ) RuntimeAssert( !!(cond) )
 //# define AssertMsg( cond, msg ) Assert( cond )
-# define AssertMsg( cond, msg ) for(;;) { if(cond) break; OutputDebugString( msg ); Assert( cond ); break; }
+# define AssertMsg( cond, msg ) for(;;) { bool b = (cond); if(b) break; OutputDebugString( msg ); Assert( b ); break; }
 #else
 # define Assert( cond )
 # define AssertMsg( cond, msg )
